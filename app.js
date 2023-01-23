b0fdc856afaaedf387410dfedba1d1e782ee9838
Sun Chuan <sunchuanop@outlook.com>
2023-01-23 12:40:30 +0100

    test push acton

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const TrelloAutomation = require('./services/TrelloAutomation');
 (async () => {
     try {
         const payload = github.context.payload;
-        core.info(`Branch name: ${process.env.BRANCH_NAME}, pull request URL: ${payload.pull_request.html_url}`);
-        var result = await TrelloAutomation.attachPullResuest(process.env.BRANCH_NAME, payload.pull_request.html_url);
+        // core.info(`Branch name: ${process.env.BRANCH_NAME}, pull request URL: ${payload.pull_request.html_url}`);
+        core.info(JSON.stringify(payload, undefined, 2));
+        // var result = await TrelloAutomation.attachPullResuest(process.env.BRANCH_NAME, payload.pull_request.html_url);
+        var result = { success: true };
         if (result.success) {
             core.info(`Successfully attached PR to card. \n ${JSON.stringify(result)}`);
         } else {
