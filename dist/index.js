e118a181c248f4c1517d9e5ab11f871f674a0b7e
Sun Chuan <sunchuanop@outlook.com>
2023-01-23 22:27:31 +0100

    attach branch to trello on create

--- dist/index.js
+++ dist/index.js
@@ -9934,15 +9934,15 @@ const TrelloAutomation = __nccwpck_require__(8474);
         var result = { success: true, msg: 'unrecognized git operation.', payload: payload };
         var branchName = process.env.BRANCH_NAME;
         if (payload.ref_type && payload.ref_type === 'branch') {
-            var branchUrl = `${payload.repository.html_url}/tree/${branchName}/`;
-            core.info(`Pushing to main branch, from: ${branchName}, branch url: ${branchUrl}`);
+            var branchUrl = `${payload.repository.html_url}/tree/${branchName}`;
+            core.info(`Created new branch, ${branchName}, branch url: ${branchUrl}`);
             var result = await TrelloAutomation.attachPullResuest(branchName, `${branchUrl}`);
         } else if (payload.pull_request) {
             core.info(`PR created, Branch name: ${branchName}, pull request URL: ${payload.pull_request.html_url}`);
             var result = await TrelloAutomation.attachPullResuest(branchName, payload.pull_request.html_url);
         }
         if (result.success) {
-            core.info(`Successfully attached PR to card. \n ${JSON.stringify(result)}`);
+            core.info(`Successfully attached URL to card. \n ${JSON.stringify(result)}`);
         } else {
             core.error(result);
             core.setFailed(result);
