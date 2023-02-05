0c6dfeeca839c191d32f9d32e01b640e5225a69d
Sunbvert <sunchuanop@outlook.com>
2023-02-05 18:58:37 +0100

    added route for uploading files

---
+++ routers/upload.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+const sendError = require('../services/SendError');
+const path = require('path');
+const fs = require('fs');
+
+const uploadPath = path.join(__dirname, '/../public/');
+
+router.post('/', async function (req, res) {
+    if (req.busboy) {
+        req.busboy.on('file', (_, file, info) => {
+            const filename = info.filename;
+            console.log(`Upload of '${filename}' started`);
+     
+            const fstream = fs.createWriteStream(path.join(uploadPath, filename));
+            file.pipe(fstream);
+     
+            fstream.on('close', () => {
+                console.log(`Upload of '${filename}' finished`);
+                res.json({ success: true, msg: `File ${filename} uploaded successfully.`})
+            });
+            fstream.on('error', (err) => {
+                sendError(res, err);
+            })
+        });
+        req.pipe(req.busboy);
+    }
+});
+
+module.exports = router
